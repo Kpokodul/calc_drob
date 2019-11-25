4bc46eeccb1d3346e7105157782fb28d88d6ed75
Kpokodul <51588090+Kpokodul@users.noreply.github.com>
2019-11-25 15:52:42 +0300

    Add files via upload

--- Петров/Кружок1/HOD.pas
+++ Петров/Кружок1/HOD.pas
@@ -1,6 +1,10 @@
 program one;
 var
-  a, b:integer;
+  a, b: integer;
+  c, d: integer;
+  nk:integer;
+  pk1:real;
+  pk2:real;
   function NOK(a, b:integer):integer;
   var NOK1:integer;
   begin
@@ -12,9 +16,22 @@ var
     else
       b := b - a;
   end;
-   NOK := NOK1 div b;
+    NOK := NOK1 div b;
+  end;
+  function PkO3(e, f, nk3:real):real;
+  begin
+    nk3 := nk3 / f;
+    PkO3 := e;
   end;
 begin
+  writeln('Введи числитель 1 и 2 дроби: ');
+  read(c, d);
+//  writeln(c, ' ', d);
+  writeln('Введи знаменатель 1 и 2 дроби: ');
   read(a, b);
-  writeln('НОК = ',NOK(a, b));
+//  writeln(a, ' ', b);
+  nk := NOK(a, b);
+  pk1 := PkO3(a, c, nk);
+  writeln( 'Надо домножитьт на ', pk1);
+//  writeln('НОК = ',NOK(a, b));
 end.
