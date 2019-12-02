ce2d8ddd74a151372bd7ff03bbbf1c7444b032b9
Kpokodul <51588090+Kpokodul@users.noreply.github.com>
2019-12-02 15:43:34 +0300

    Add files via upload

--- Петров/Кружок1/HOD.pas
+++ Петров/Кружок1/HOD.pas
@@ -2,6 +2,7 @@ program one;
 var
   a, b: integer;
   c, d: integer;
+  h: real;
   nk:integer;
   pk1:real;
   pk2:real;
@@ -18,20 +19,43 @@ var
   end;
     NOK := NOK1 div b;
   end;
-  function PkO3(e, f, nk3:real):real;
+  function PkO3(e, f, nk3:integer):real;
   begin
-    nk3 := nk3 / f;
+    nk3 := nk3 div f;
+    e := e * nk3;
     PkO3 := e;
   end;
+  function SD(j, k:real):real;
+  begin
+    writeln('Пока тут пусто, поэтому печатаю.');
+    writeln('Ответ: ');
+    writeln(j);
+    writeln('--');
+    writeln(k);
+  end;
 begin
-  writeln('Введи числитель 1 и 2 дроби: ');
-  read(c, d);
-//  writeln(c, ' ', d);
-  writeln('Введи знаменатель 1 и 2 дроби: ');
+  writeln('Введи числитель и знаменатель 1 дроби: ');
   read(a, b);
-//  writeln(a, ' ', b);
-  nk := NOK(a, b);
-  pk1 := PkO3(a, c, nk);
-  writeln( 'Надо домножитьт на ', pk1);
-//  writeln('НОК = ',NOK(a, b));
+  writeln('Введи числитель и знаменатель 2 дроби: ');
+  read(c, d);
+  nk := NOK(b, d);
+//  writeln('НОК = ',nk);
+  pk1 := PkO3(a, b, nk);
+  pk2 := PkO3(c, d, nk);
+  b := nk;
+  d := nk;
+//  writeln( 'Числитель первой дроби после домножениея равен ', pk1);
+//  writeln( 'Знаменатель первой дроби после домножениея равен ', b);
+//  writeln( 'Числитель второй первой дроби после домножениея равен ', pk2);
+//  writeln( 'Знаменатель первой дроби после домножениея равен ', d);
+  h := pk1 + pk2;
+  if h > nk then
+    SD(h, nk)
+  else
+  begin
+    writeln('Ответ: ');
+    writeln(h);
+    writeln('--');
+    writeln(nk);
+  end;
 end.
